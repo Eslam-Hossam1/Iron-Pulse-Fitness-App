cfea4f09a87f98636fafa808d785c2b199e1a182
Eslam Hossam <eslamhossam.tech@gmail.com>
2026-04-18 14:12:08 +0200

    refactor: extract MyApp to separate file and rename to FitnessApp

---
+++ lib/fitness_app.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class FitnessApp extends StatelessWidget {
+  const FitnessApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(debugShowCheckedModeBanner: false);
+  }
+}
