9f03238e702be57071251d8aecbb3c932b50d787
Mohamed Ashraf <m.ashrafelbarbary@gmail.com>
2026-04-20 17:02:29 +0200

    adding supabase login service

---
+++ lib/features/home_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HomeView extends StatelessWidget {
+  const HomeView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(onPressed: () {}, icon: Icon(Icons.arrow_back)),
+      ),
+      body: Center(child: Text('home view')),
+    );
+  }
+}
